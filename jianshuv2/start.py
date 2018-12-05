74bea1e830af486240f75d86d9fba7532c5aee2d
jiuguai <jijiyezero@163.com>
2018-12-05 15:28:02 +0800

    update yy

--- jianshuv2/start.py
+++ jianshuv2/start.py
@@ -1,4 +1,6 @@
 from scrapy import cmdline
+
 cmdline.execute('scrapy crawl js2'.split())
+# cmdline.execute('scrapy crawl js'.split())
 
 # lpush js2:start_urls https://www.jianshu.com/trending/monthly?utm_medium=index-banner-s&utm_source=desktop
